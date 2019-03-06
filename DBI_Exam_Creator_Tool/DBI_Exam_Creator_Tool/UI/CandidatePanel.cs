23327cf505fea368b02ba3104e278687ba746353
Minh Hiếu Phạm <pmh.ars14@gmail.com>
2019-03-06 09:31:33 +0700

    fix DML state

--- DBI_Exam_Creator_Tool/DBI_Exam_Creator_Tool/UI/CandidatePanel.cs
+++ DBI_Exam_Creator_Tool/DBI_Exam_Creator_Tool/UI/CandidatePanel.cs
@@ -183,9 +183,8 @@ namespace DBI_Exam_Creator_Tool.UI
 
         private void dmlState()
         {
-            testQueryTxt.Enabled = false;
-
-
+            testQueryTxt.Enabled = true;
+            
             requireSortCheckBox.Checked = requireSortCheckBox.Visible = false;
 
             dbNameLabel.Visible = DBNameTxt.Visible = false;
